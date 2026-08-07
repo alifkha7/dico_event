89a7ad7032f55d3ab575afb289b94b7e15548b53
Alif Khaerudin <alifkha7@live.com>
2026-08-07 11:26:28 +0700

    feat: map root URL to Swagger UI documentation

--- event_project/urls.py
+++ event_project/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("api/login/", TokenObtainPairView.as_view()),
     path("api/token/", TokenRefreshView.as_view()),
     # API Documentation
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui-root"),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
