89dc4f9eb9f7b0983bef37085687e151979a60fd
Alif Khaerudin <alifkha7@live.com>
2026-08-07 10:52:01 +0700

    fix: add STATIC_ROOT to common.py to fix collectstatic crash

--- event_project/settings/common.py
+++ event_project/settings/common.py
@@ -125,6 +125,8 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
